5766fce01de5b8ba49b22c0033cdc3f071cdb745
Luop-code <1656483310@qq.com>
2022-03-31 17:58:09 +0800

    ref

---
+++ Vue2/demo-3/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+   <h1 ref="myh1">App根组件 --- </h1>
+    <button @click="showThis">打印this</button>
+    <button @click="onReset">重置Left组件的count值</button>
+   <br>
+
+   <div class="box">
+     <Left ref="comLeft"></Left>
+   </div>
+  </div>
+</template>
+
+<script>
+import Left from '@/components/Left.vue'
+
+export default {
+  methods:{
+    showThis(){
+      // console.log(this.$refs.myh1);
+      this.$refs.myh1.style.color = 'red'
+    },
+    onReset(){
+      
+      // this.$refs.comLeft.count= 0
+      this.$refs.comLeft.resetCount()
+    }
+  },
+  components:{
+    Left,
+
+  }
+}
+</script>
+
+<style lang="less" >
+#app {
+padding: 1px 20px 20px;
+background-color: #efefef;
+}
+</style>
---
+++ Vue2/demo-3/src/components/Left.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="left-container">
+   <h3>Left组件--------{{ count }}</h3>
+   <button @click="count+=1">+1</button>
+   <button @click="resetCount">重置</button>
+  </div>
+</template>
+
+<script>
+// 导入 eventBus.js模块
+
+export default {
+    data(){
+        return {
+            count:0
+        }
+    },
+    methods: {
+        resetCount(){
+            this.count = 0
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+// scoped，原理就是，自动为每个标签添加 data-v-xxx的属性，这样就不会样式冲突
+.left-container {
+  
+    flex: 1;
+    padding:0 20px 20px;
+    background-color: orange;
+    min-height: 250px;
+ 
+}
+
+</style>
